16fb3a9d197cf9366178edd4c6412dab365f5554
Messi <ruchan111000@gmail.com>
2021-09-08 16:59:05 +0545

    Ruchan's commit

--- K#.py
+++
@@ -1,9 +0,0 @@
-a=float(input("Enter a number:"))
-b=float(input('Enter another number:'))
-c=float(input('Enter third number:'))
-d=float(input('Enter fourth number:'))
-
-if a==b and c==d :
-    print('Hello')
-else:
-    print('Program End')
